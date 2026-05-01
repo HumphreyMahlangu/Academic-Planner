c636ec54400d5847a623c6d668a1804e439b5f0a
Samkelo Mahlangu <godasamukelo757@gmail.com>
2026-05-01 19:29:25 +0200

    Initial commit

---
+++ src/main/java/za/ac/mycput/academicplanner/AcademicPlannerApplication.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.academicplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/mycput/academicplanner/AcademicPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.academicplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
